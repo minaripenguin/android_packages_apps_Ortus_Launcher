a7d7475b6d99e435b43072a485e007aa6c2755a9
Thecrazyskull <anaskarbila@gmail.com>
2017-02-10 09:38:50 -0500

    StatixLauncher: double tap on home screen to turn off screen
    Change-Id: I554c005d5e523aca0842c78a353686e86af1a7f2
    Reviewed-on: https://review.statixos.com/c/android_packages_apps_Statix_Launcher/+/8454
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

--- src/com/statix/launcher/touch/StatixWorkspaceTouchListener.java
+++ src/com/statix/launcher/touch/StatixWorkspaceTouchListener.java
@@ -1,13 +1,26 @@
 package com.statix.launcher.touch;
 
+import android.content.Context;
+import android.os.PowerManager;
+import android.view.MotionEvent;
+
 import com.android.launcher3.Launcher;
 import com.android.launcher3.Workspace;
 import com.android.launcher3.touch.WorkspaceTouchListener;
 
 public class StatixWorkspaceTouchListener extends WorkspaceTouchListener {
 
+    private final PowerManager mPowerManager;
+
     public StatixWorkspaceTouchListener(Launcher launcher, Workspace<?> workspace) {
         super(launcher, workspace);
+        mPowerManager = (PowerManager) workspace.getContext().getSystemService(Context.POWER_SERVICE);
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        mPowerManager.goToSleep(event.getEventTime());
+        return true;
     }
 
 }
